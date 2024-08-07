7049d980ae8e602fa38e55964bd81aa862c8def9
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2024-08-07 14:55:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from handlers import register_handlers
 from config import API_ID, API_HASH, BOT_TOKEN
-from schedule import start_scheduler
+from subscription_scheduler import start_scheduler  # Updated import
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
