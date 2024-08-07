156f648e499a16821bf49a8b26a4e03d3e6b630b
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2024-08-07 14:51:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from pyrogram import Client
 from handlers import register_handlers
 from config import API_ID, API_HASH, BOT_TOKEN
+from schedule import start_scheduler
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 def main():
     register_handlers(app)
+    start_scheduler(app)
     app.run()
 
 if __name__ == "__main__":
