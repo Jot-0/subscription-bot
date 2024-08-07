a853a2a4a020f561c4cf2ff456fce3fa4df0c7b8
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2024-08-07 15:19:50 +0530

    Update subscription_scheduler.py

--- subscription_scheduler.py
+++ subscription_scheduler.py
@@ -10,11 +10,11 @@ def check_subscriptions(client: Client):
         for user_id, user in list(subscribed_users.items()):
             plan_end_date = datetime.strptime(user['plan_end_date'], "%d/%m/%Y").date()
             if plan_end_date == current_date:
-                client.send_message(user_id, f"Your plan ends today.\nStart Date: {user['start_date']}\nEnd Date: {user['plan_end_date']}")
+                client.send_message(user_id, f"Your plan ends today.please renew your plan nor and for renew contact @Sam_Dude2.\nStart Date: {user['start_date']}\nEnd Date: {user['plan_end_date']}")
                 client.send_message(OWNER_ID, f"User {user['first_name']} ({user_id})'s plan ends today.\nStart Date: {user['start_date']}\nEnd Date: {user['plan_end_date']}")
                 print(f"DEBUG: Sent end date notification for user_id: {user_id}")
             elif plan_end_date == current_date + timedelta(days=2):
-                client.send_message(user_id, f"Your plan will end in 2 days.\nStart Date: {user['start_date']}\nEnd Date: {user['plan_end_date']}")
+                client.send_message(user_id, f"Your plan will end in 2 days.please renew your plan after 2 days and for renew contact @Sam_Dude2.\nStart Date: {user['start_date']}\nEnd Date: {user['plan_end_date']}")
                 client.send_message(OWNER_ID, f"User {user['first_name']} ({user_id})'s plan will end in 2 days.\nStart Date: {user['start_date']}\nEnd Date: {user['plan_end_date']}")
                 print(f"DEBUG: Sent 2 days before end date notification for user_id: {user_id}")
         time.sleep(86400)  # Check once a day
