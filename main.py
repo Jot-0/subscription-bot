61687d9f95f2e448ef785f0e479372dd68ad0462
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2024-08-07 13:05:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-import logging
-from pyrogram import Client, idle
+from pyrogram import Client
 from handlers import register_handlers
-from scheduler import run_scheduler
-
-# Configure logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-# Your API ID, API hash, and bot token
 from config import API_ID, API_HASH, BOT_TOKEN
 
-# Create a Pyrogram Client
-app = Client("subscription_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+def main():
+    register_handlers(app)
+    app.run()
 
 if __name__ == "__main__":
-    # Register command and message handlers
-    register_handlers(app)
-
-    # Start the Pyrogram client
-    app.start()
-
-    # Run the scheduler in a separate thread and pass the app instance
-    import threading
-    scheduler_thread = threading.Thread(target=run_scheduler, args=(app,))
-    scheduler_thread.start()
-
-    # Run the bot
-    idle()
+    main()
