b7d5f4057ed2f65e2c356f03e6c821fc4b0e5fe9
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2024-08-08 13:23:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_ID = '7048752'
-API_HASH = '4d81d01ce78778b03dc6d13ee19ec3ea'
-BOT_TOKEN = '7311064538:AAFz5ATX4PPs6DhJGgkvwlYLaY4oKBvKQ_Y'
-OWNER_ID = int('6601250340')  # Replace with your Telegram user ID
+API_ID = ''
+API_HASH = ''
+BOT_TOKEN = ''
+OWNER_ID = int('')  # Replace with your Telegram user ID
